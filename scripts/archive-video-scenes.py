325404f41c460b263470d931b763e832dad4a801
Joshua Eldridge <jeldridge@scrippsnetworks.com>
2018-04-25 17:41:21 -0400

    mostly working commit .. moving core functions to a helper script

--- scripts/archive-video-scenes.py
+++ scripts/archive-video-scenes.py
@@ -10,87 +10,118 @@ import os
 import sys
 import argparse
 import random
-import hashlib
+
+from helper_functions import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename', help='name of the video file. example: 20111022144212.mov')
 parser.add_argument('-v', '--verbose', default=True)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-f', '--filename', help='name of the video file. example: 20111022144212.mov')
+group.add_argument('-d', '--dir',  help='path to a directory. example: /Volumes/2TB-WD-Elements/DV Library Backup/')
+group.add_argument('-l', '--list',  help='list of files to process. example: files-to-process.txt')
+
 args = parser.parse_args()
-video_file = args.filename
-file_basename = os.path.splitext(video_file)[0]
 
-def check_lib(name):
-    from distutils.spawn import find_executable
-    return find_executable(name) is not None
-    
-# First check that all permissions are valid:
-# 0. Required libraries (ffmpeg, ffprobe) are installed and available
-# 1. File is readable and directory is writable, so we can create a subfolder for assets
-# 2.
-
-# 0. Check for required libraries
-if check_lib('ffmpeg') and check_lib('ffprobe') is None:
-    sys.exit('Error: This script requires ffmpeg and ffprobe libraries be available in the $PATH!')
-
-# 1. Check for permissions
-if not os.access(os.getcwd(), os.W_OK) or not os.access(video_file, os.R_OK):
-    sys.exit('Error: Directory is writable and file is readable!')
-
-def md5(fname):
-    hash_md5 = hashlib.md5()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-    
-s_duration = subprocess.check_output(['ffprobe', '-i', video_file, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ('p=0')])
-f_duration = float(s_duration.strip())
-i_duration = int(f_duration)
-
-# Based on the duration of the video, determine the number of segments
-# Minimally, create two segments twenty at most, otherwise every three minutes
-
-
-if args.verbose is True:
-    print('Video File Name: %s' % video_file)
-    print('Float Duration: %f' % f_duration)
-    print('Int Duration: %d' % i_duration)
-
+hash_cache_file = '/Users/161619/Git/VideoManagement/scripts/video_hashes.cache'
 
 # FFMPEG Settings
 clip_duration=5
 clip_fps=10
 clip_scale=320
 
-# Create the subdirectory for the resulting GIF files and metadata
-os.mkdir(file_basename)
-    
-file_part=1
+video_directory = None
+files_list = None
+video_file = None
+file_basename = None
 
+# Check that the file or folder is actually valid and readable/writable.
+if args.dir is not None:
+    if os.path.isdir(args.dir) and os.access(args.dir, os.W_OK):
+        video_directory = args.dir
+        files_list = find_my_files(video_directory)
+    else:
+        sys.exit('Error: Directory is not valid or is not writable!')
+
+if args.filename is not None:
+    if os.path.isfile(args.filename) and os.access(args.filename, os.R_OK):
+        video_file = args.filename
+        file_basename = os.path.splitext(video_file)[0]
+    else:
+            sys.exit('Error: File is not valid or not readable!')
+
+if args.list is not None:
+    if os.path.isfile(args.list) and os.access(video_file, os.R_OK):
+        with open(args.list, 'r') as f:
+            files_list = [line.rstrip() for line in f]
+    else:
+            sys.exit('Error: File is not valid or not readable!')
+
+# Next check that all libraries are available and installed (ffmpeg, ffprobe)
+# and the the file and folder permissions are valid - need to write back
+if check_lib('ffmpeg') and check_lib('ffprobe') is None:
+    sys.exit('Error: This script requires ffmpeg and ffprobe libraries be available in the $PATH!')
+
+# One of the first things we should do is to check the global list of assets
+# to see if we even need to process the file. The first step is to look for a local
+# version of the cache file. TO DO: Pull the file from a remote location.
+hash_set = load_hash_cache(hash_cache_file)
+
+# Create the subdirectory for the resulting GIF files and metadata
+# This is the process for a single file:
+if video_file is not None:
+    if not os.path.exists(file_basename):
+        os.mkdir(file_basename)
+    md5 = dump_md5(video_file)
+    if md5 in hash_set:
+        sys.exit('Aborting! This file has already been processed')
+
+    detect_scenes(video_file, file_basename)
+    create_thumbs(video_file, clip_duration = clip_duration, clip_fps = clip_fps, clip_scale = clip_scale)
+    dump_md5(video_file, file_basename)
+    dump_video_metadata(video_file, file_basename)
+else:
+    for file in files_list:
+        print file
+
+sys.exit()   
+
+    
+# s_duration = subprocess.check_output(['ffprobe', '-hide_banner', '-i', video_file, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ('p=0')])
+# f_duration = float(s_duration.strip())
+# i_duration = int(f_duration)
+# 
+# if args.verbose is True:
+#     print('Video File Name: %s' % video_file)
+#     print('Float Duration: %f' % f_duration)
+#     print('Int Duration: %d' % i_duration)
+
+
+
+
+# if this is a single file, process it an exit
+
+
+
+# if this is a list of files 
+
+file_part=1
 if os.path.getsize('%s.ts' % file_basename) > 0:
     with open('%s.ts' % file_basename, 'r') as f:
         for timestamp in f:
             pad = '%03d' % file_part
-            subprocess.call(['ffmpeg', '-y', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-vf', 'fps=%s,scale=%s:-1:flags=lanczos,palettegen' % (clip_fps, clip_scale), 'palette-%s.png' % (pad)])
-            subprocess.call(['ffmpeg', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-i', 'palette-%s.png' % (pad), '-filter_complex', 'fps=%s,scale=%s:-1:flags=lanczos[x];[x][1:v] paletteuse' % (clip_fps, clip_scale), '%s/%s-%s.gif' % (file_basename, file_basename, pad)])
+            subprocess.call(['ffmpeg', '-hide_banner', '-y', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-vf', 'fps=%s,scale=%s:-1:flags=lanczos,palettegen' % (clip_fps, clip_scale), 'palette-%s.png' % (pad)])
+            subprocess.call(['ffmpeg', '-hide_banner', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-i', 'palette-%s.png' % (pad), '-filter_complex', 'fps=%s,scale=%s:-1:flags=lanczos[x];[x][1:v] paletteuse' % (clip_fps, clip_scale), '%s/%s-%s.gif' % (file_basename, file_basename, pad)])
             os.remove('palette-%s.png' % pad)
-            subprocess.call(['ffmpeg', '-ss', '%f' % float(timestamp), '-i', video_file, '-vframes', '1', '-vf', 'scale=%s:-1' % (clip_scale), '%s/%s-%s.png' % (file_basename, file_basename, pad)])
+            subprocess.call(['ffmpeg', '-hide_banner', '-ss', '%f' % float(timestamp), '-i', video_file, '-vframes', '1', '-vf', 'scale=%s:-1' % (clip_scale), '%s/%s-%s.png' % (file_basename, file_basename, pad)])
             file_part += 1
 else:
-    timestamp = 1
+    timestamp = 5
     pad = '%03d' % file_part
-    subprocess.call(['ffmpeg', '-y', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-vf', 'fps=%s,scale=%s:-1:flags=lanczos,palettegen' % (clip_fps, clip_scale), 'palette-%s.png' % (pad)])
-    subprocess.call(['ffmpeg', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-i', 'palette-%s.png' % (pad), '-filter_complex', 'fps=%s,scale=%s:-1:flags=lanczos[x];[x][1:v] paletteuse' % (clip_fps, clip_scale), '%s/%s-%s.gif' % (file_basename, file_basename, pad)])
+    subprocess.call(['ffmpeg', '-hide_banner', '-y', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-vf', 'fps=%s,scale=%s:-1:flags=lanczos,palettegen' % (clip_fps, clip_scale), 'palette-%s.png' % (pad)])
+    subprocess.call(['ffmpeg', '-hide_banner', '-ss', '%f' % float(timestamp), '-t', str(clip_duration), '-i', video_file, '-i', 'palette-%s.png' % (pad), '-filter_complex', 'fps=%s,scale=%s:-1:flags=lanczos[x];[x][1:v] paletteuse' % (clip_fps, clip_scale), '%s/%s-%s.gif' % (file_basename, file_basename, pad)])
     os.remove('palette-%s.png' % pad)
-    subprocess.call(['ffmpeg', '-ss', '%f' % float(timestamp), '-i', video_file, '-vframes', '1', '-vf', 'scale=%s:-1' % (clip_scale), '%s/%s-%s.png' % (file_basename, file_basename, pad)])
+    subprocess.call(['ffmpeg', '-hide_banner', '-ss', '%f' % float(timestamp), '-i', video_file, '-vframes', '1', '-vf', 'scale=%s:-1' % (clip_scale), '%s/%s-%s.png' % (file_basename, file_basename, pad)])
 
-# Dump all the video metadata to a json file
-video_metadata = '%s/%s.json' % (file_basename, file_basename)
-with open(video_metadata, 'w') as f:
-    json_output = subprocess.check_output(['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', video_file])
-    f.write(json_output)
+dump_md5(video_file, file_basename)
+dump_video_metadata(video_file, file_basename)
 
-video_md5_file = '%s/%s.md5' % (file_basename, file_basename)
-video_md5_hash = md5(video_file)
-with open(video_md5_file, 'w') as f:
-    f.write(video_md5_hash)
